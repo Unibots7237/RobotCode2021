40ead1e91b263b660df7166723e93d64b6521bcb
adam3957 <74639539+adam3957@users.noreply.github.com>
2020-11-18 21:00:59 -0600

    Add files via upload

---
+++ Objects.java
@@ -0,0 +1,7 @@
+public class Computer {
+    public static void main(String args[]) {
+        Computer Windows = new Computer();
+        Computer HP = new Computer();
+        Computer Mac = new Computer();
+    }
+}
